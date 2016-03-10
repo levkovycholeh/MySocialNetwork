3e7d881139bb10da1dc3ce85155aa2e4e51ba667
levkovycholeh <levkovych.oleh@gmail.com>
2016-03-10 14:22:40 +0200

    Update SessionUserDao.java

--- src/main/java/com/bionic/socialNetwork/dao/SessionUserDao.java
+++ src/main/java/com/bionic/socialNetwork/dao/SessionUserDao.java
@@ -2,10 +2,7 @@ package com.bionic.socialNetwork.dao;
 
 import com.bionic.socialNetwork.models.SessionUser;
 
-/**
- * @author Denis
- * @version 1.00  18.07.2014.
- */
+
 public interface SessionUserDao {
 
     public void insert(SessionUser sessionUser) throws Exception;
