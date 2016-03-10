ec286c477211e4089028a8597e0fdc754eb9b37f
levkovycholeh <levkovych.oleh@gmail.com>
2016-03-10 14:21:54 +0200

    Update PostDao.java

--- src/main/java/com/bionic/socialNetwork/dao/PostDao.java
+++ src/main/java/com/bionic/socialNetwork/dao/PostDao.java
@@ -6,12 +6,7 @@ import com.bionic.socialNetwork.models.User;
 
 import java.util.List;
 
-/**
- * Posts Dao
- *
- * @author Denis Biyovskiy
- * @version 1.00  16.07.2014.
- */
+
 public interface PostDao {
     public Post selectById(long userId) throws Exception;
     public void insert(Post post) throws Exception;
